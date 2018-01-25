f3c7cff63d4360b001c85f1b9ad4e7f0eb1ea832
Larry Song <Larry.Song@civica.com.au>
2018-01-25 13:29:51 +1100

    first commit

---
+++ Compare-2Folders.ps1
@@ -0,0 +1,144 @@
+
+function Compare-2Folders
+{
+    PARAM(
+        [string]$Path1,
+        [string]$Path2,
+        [switch]$Directory,
+        [switch]$File = $true,
+        [switch]$UseLastModifiedDateInsteadOfHash,
+        [switch]$OutputDifferenceOnly = $true,
+        [switch]$OutputFullName
+    )
+
+    $f1 = Get-Item -Path $Path1.TrimEnd('\') -ErrorAction:SilentlyContinue
+    if(!$f1 -or $f1.Mode -notmatch '^d')
+    {
+        Throw "'$Path1' is not a folder"
+    }
+    $s1_file = Get-ChildItem -Path $f1.FullName -File -Recurse
+    $s1_directory = Get-ChildItem -Path $f1.FullName -Directory -Recurse
+
+    $f2 = Get-Item -Path $Path2.TrimEnd('\') -ErrorAction:SilentlyContinue
+    if(!$f2 -or $f2.Mode -notmatch '^d')
+    {
+        Throw "'$Path2' is not a folder"
+    }
+    $s2_file = Get-ChildItem -Path $f2.FullName -File -Recurse
+    $s2_directory = Get-ChildItem -Path $f2.FullName -Directory -Recurse
+
+    $obj = New-Object PSObject -Property @{"#1" = $null; "#2" = $null; "State" = $null; "Type" = $null}
+
+    if($File)
+    {
+        $h1 = @{}
+        $h2 = @{}
+        if($UseLastModifiedDateInsteadOfHash)
+        {
+            $s1_file | %{
+                $h1[$($_.FullName.Replace($_.DirectoryName, ""))] = $_.LastWriteTimeUtc.Ticks
+            }
+            $s2_file | %{
+                $h2[$($_.FullName.Replace($_.DirectoryName, ""))] = $_.LastWriteTimeUtc.Ticks
+            }
+        }
+        else
+        {
+            $s1_file | %{
+                $h1[$($_.FullName.Replace($_.DirectoryName, ""))] = (Get-FileHash -Path $_.FullName).Hash
+            }
+            $s2_file | %{
+                $h2[$($_.FullName.Replace($_.DirectoryName, ""))] = (Get-FileHash -Path $_.FullName).Hash
+            }
+        }
+
+        $h1.Keys + $h2.Keys | Sort-Object -Unique | %{
+            if($h1.ContainsKey($_) -and $h2.ContainsKey($_))
+            {
+                if($h1[$_] -eq $h2[$_]) # file exists on both place
+                {
+                    if(!$OutputDifferenceOnly)
+                    {
+                        $NewObj = $obj.PSObject.Copy()
+                        $NewObj.'#1' = if($OutputFullName){"$($f1.FullName)$_"}else{$_}
+                        $NewObj.'#2' = if($OutputFullName){"$($f2.FullName)$_"}else{$_}
+                        $NewObj.'State' = 'Identical'
+                        $NewObj.'Type' = 'File'
+                        $NewObj
+                    }
+                }
+                else
+                {
+                    $NewObj = $obj.PSObject.Copy()
+                    $NewObj.'#1' = if($OutputFullName){"$($f1.FullName)$_"}else{$_}
+                    $NewObj.'#2' = if($OutputFullName){"$($f2.FullName)$_"}else{$_}
+                    $NewObj.'State' = 'Different'
+                    $NewObj.'Type' = 'File'
+                    $NewObj
+                }
+            }
+            elseif($h1.ContainsKey($_) -and !$h2.ContainsKey($_)) # file exists on path1
+            {
+                $NewObj = $obj.PSObject.Copy()
+                $NewObj.'#1' = if($OutputFullName){"$($f1.FullName)$_"}else{$_}
+                #$NewObj.'#2' = ""
+                $NewObj.'State' = '#1'
+                $NewObj.'Type' = 'File'
+                $NewObj
+            }
+            elseif(!$h1.ContainsKey($_) -and $h2.ContainsKey($_)) # file exists on path2
+            {
+                $NewObj = $obj.PSObject.Copy()
+                #$NewObj.'#1' = ""
+                $NewObj.'#2' = if($OutputFullName){"$($f2.FullName)$_"}else{$_}
+                $NewObj.'State' = '#2'
+                $NewObj.'Type' = 'File'
+                $NewObj
+            }
+        }
+    }
+
+    if($Directory)
+    {
+        $h1 = @{}
+        $h2 = @{}
+        $s1_directory | %{
+            $h1[$($_.FullName.Replace($_.Parent.FullName, ""))] = ""
+        }
+        $s2_directory | %{
+            $h2[$($_.FullName.Replace($_.Parent.FullName, ""))] = ""
+        }
+        $h1.Keys + $h2.Keys | Sort-Object -Unique | %{
+            if($h1.ContainsKey($_) -and $h2.ContainsKey($_))
+            {
+                if(!$OutputDifferenceOnly)
+                {
+                    $NewObj = $obj.PSObject.Copy()
+                    $NewObj.'#1' = if($OutputFullName){"$($f1.FullName)$_"}else{$_}
+                    $NewObj.'#2' = if($OutputFullName){"$($f2.FullName)$_"}else{$_}
+                    $NewObj.'State' = '#1 #2'
+                    $NewObj.'Type' = 'Directory'
+                    $NewObj
+                }
+            }
+            elseif($h1.ContainsKey($_) -and !$h2.ContainsKey($_)) # directory exists on path1
+            {
+                $NewObj = $obj.PSObject.Copy()
+                $NewObj.'#1' = if($OutputFullName){"$($f1.FullName)$_"}else{$_}
+                #$NewObj.'#2' = ""
+                $NewObj.'State' = '#1'
+                $NewObj.'Type' = 'Directory'
+                $NewObj
+            }
+            elseif(!$h1.ContainsKey($_) -and $h2.ContainsKey($_)) # directory exists on path2
+            {
+                $NewObj = $obj.PSObject.Copy()
+                #$NewObj.'#1' = ""
+                $NewObj.'#2' = if($OutputFullName){"$($f2.FullName)$_"}else{$_}
+                $NewObj.'State' = '#2'
+                $NewObj.'Type' = 'Directory'
+                $NewObj
+            }
+        }
+    }
+}
